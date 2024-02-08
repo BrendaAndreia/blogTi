dd9dc795bc562334619ee37d9d2303c726033e31
brendaandreia <brendaandreia@gmail.com>
2024-02-07 23:11:54 -0300

    feat: login com github

--- blog/settings.py
+++ blog/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.github',
 
 ]
 
