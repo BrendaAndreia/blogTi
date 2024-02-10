86728b439c780f34f30c0e35d3175336d0ef9e5b
brendaandreia <brendaandreia@gmail.com>
2024-02-09 21:02:04 -0300

    refactoring blog

--- blogTi/admin.py
+++ blogTi/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from . import models
+from .models import Post, Topico, Tag, Assunto,   Comentario, PostSituacao, Situacao
 
-admin.site.register(models.Post)
+
+class TopicoInline(admin.TabularInline):
+    model = Topico
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        TopicoInline
+    ]
+    class Meta:
+        model = Post
+        
+        
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Comentario)
+admin.site.register(Assunto)
+admin.site.register(PostSituacao)
+admin.site.register(Situacao)
+admin.site.register(Topico)
--- blogTi/models.py
+++ blogTi/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from datetime import datetime
 from ckeditor_uploader.fields import RichTextUploadingField
 from ckeditor.fields import RichTextField
+from contas.models import Perfil
 
 class Post(models.Model):
-    autor = models.CharField(max_length=255)
+    autor = models.ForeignKey(Perfil, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
-    resumo = RichTextField(blank=True, null=True)
-    conteudo = RichTextUploadingField()
     image_capa = models.ImageField(upload_to='static/blog/%Y/%m/%d/', blank=True, null=True)
     data_publicacao = models.DateTimeField(default=datetime.now())
 
@@ -16,3 +15,43 @@ class Post(models.Model):
     def __str__(self):
         return self.titulo
     
+class Topico(models.Model):
+    conteudo = RichTextUploadingField(blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+class Tag(models.Model):
+    nome = models.CharField(max_length=50)
+    post = models.ManyToManyField(Post)
+    
+    def __str__(self):
+        return self.nome
+    
+class Assunto(models.Model):
+    nome = models.CharField(max_length=150)
+    post = models.ManyToManyField(Post)
+    
+    def __str__(self):
+        return self.nome
+
+class Situacao(models.Model):
+    nome = models.CharField(max_length=50)
+    posts = models.ManyToManyField(Post, through='PostSituacao', through_fields=('situacao', 'post'))
+    
+    def __str__(self):
+        return self.nome
+    
+class PostSituacao(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    situacao = models.ForeignKey(Situacao, on_delete=models.CASCADE)
+    data = models.DateField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ['post', 'situacao']
+        
+class Comentario(models.Model):
+    texto = models.TextField(max_length=1024)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.texto
