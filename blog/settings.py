fd43ddf283e344c705377c3d597385a4262a5e26
brendaandreia <brendaandreia@gmail.com>
2024-02-12 21:56:19 -0300

    config bd_heroku

--- blog/settings.py
+++ blog/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
           'HOST': 'ec2-34-236-199-229.compute-1.amazonaws.com',
           'PORT': '5432',
           'OPTIONS': {
-                    'options': '-c search_path=blog_db'
+                    'options': '-c search_path=public'
           }
      }
 }
@@ -140,17 +140,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+#STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
-MEDIA_URL = '/image/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+#MEDIA_URL = '/image/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/')
-]
+STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, 'staticfiles/')
+#]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
