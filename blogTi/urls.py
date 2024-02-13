62331f5ea59c32879db51165f1ed11db4c125cdd
brendaandreia <brendaandreia@gmail.com>
2024-02-12 22:05:30 -0300

    url

--- blogTi/urls.py
+++ blogTi/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog/', views.blog, name='blog'),
-    path('post/<int:post_id>/post_detail', views.post_detail, name='post_detail'),
-    path('posts/<int:post_id>/comentarios', views.comentar, name='comentarios'),
-    path('posts/comentarios/<int:post_id>', views.comentar_post, name='comentar_post_htmx'),
-    path('posts/comentarios/deletar/<int:comentario_id>', views.deletar_comentario, name='deletar_comentario_htmx'),
-    path('posts/comentarios/editar/<int:comentario_id>', views.editar_comentario, name='editar_comentario_htmx'),
+    path('/blog/', views.blog, name='blog'),
+    path('/post/<int:post_id>/post_detail', views.post_detail, name='post_detail'),
+    path('/posts/<int:post_id>/comentarios', views.comentar, name='comentarios'),
+    path('/posts/comentarios/<int:post_id>', views.comentar_post, name='comentar_post_htmx'),
+    path('/posts/comentarios/deletar/<int:comentario_id>', views.deletar_comentario, name='deletar_comentario_htmx'),
+    path('/posts/comentarios/editar/<int:comentario_id>', views.editar_comentario, name='editar_comentario_htmx'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
